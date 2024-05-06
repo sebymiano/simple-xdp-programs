c96e41b9f783662ffb283c3210711ecfdfd57a44
Sebastiano Miano <mianosebastiano@gmail.com>
2024-05-06 11:42:47 +0100

    Update scripts

--- profiles/setup-dut.sh
+++ profiles/setup-dut.sh
@@ -208,7 +208,7 @@ PACKAGES+=" libnuma-dev libelf-dev libcap-dev libpcap-dev libjansson-dev libipse
 PACKAGES+=" autoconf libcsv-dev libbfd-dev"                                                                  # DPDK burst replay
 PACKAGES+=" pciutils build-essential cmake linux-headers-$(uname -r) libnuma-dev libtbb2"                    # Moongen
 PACKAGES+=" tmux texlive-font-utils pdf2svg poppler-utils pkg-config net-tools bash tcpreplay"               # utility libraries
-PACKAGES+=" gnuplot"                                                                                         # for generating figures
+PACKAGES+=" gnuplot gcc-12"                                                                                         # for generating figures
 
 $SUDO bash -c "DEBIAN_FRONTEND=noninteractive apt-get install -yq $PACKAGES"
 
